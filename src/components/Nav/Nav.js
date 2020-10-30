79147bcfdd10a79243aa4fe54d68da47e65ce3da
Tou Xiong <touyeexiong@gmail.com>
2020-10-30 00:19:05 -0500

    added nav bar from material ui. Need to float login/register Link to the right

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,6 +4,12 @@ import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Nav = (props) => {
   let loginLinkData = {
@@ -17,6 +23,39 @@ const Nav = (props) => {
   }
 
   return (
+    <>
+    <AppBar postion="static">
+      <Toolbar>
+        <IconButton edge="start" color ="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">
+            <Link to="/home">
+              <h2 className="nav-title">Smokin' Ninjas</h2>
+            </Link>
+        </Typography>
+            <Link className="nav-link" to={loginLinkData.path}>
+              {/* Show this link if they are logged in or not,
+          but call this link 'Home' if they are logged in,
+          and call this link 'Login / Register' if they are not */}
+              {loginLinkData.text}
+            </Link>
+            {/* Show the link to the info page and the logout button if the user is logged in */}
+            {props.store.user.id && (
+              <>
+                <Link className="nav-link" to="/info">
+                  Info Page
+            </Link>
+                <LogOutButton className="nav-link" />
+              </>
+            )}
+            {/* Always show this link since the about page is not protected */}
+            {/* <Link className="nav-link" to="/about">
+          About
+        </Link> */}
+          {/* <Button>Testing</Button> */}
+      </Toolbar>
+    </AppBar>
     <div className="nav">
       <Link to="/home">
         <h2 className="nav-title">Smokin' Ninjas</h2>
@@ -43,6 +82,7 @@ const Nav = (props) => {
         </Link> */}
       </div>
     </div>
+    </>
   );
 };
 
