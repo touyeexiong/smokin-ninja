d6fa01bf0dd1ef2ea8efd78c7d45d78468a155d6
Tou Xiong <touyeexiong@gmail.com>
2020-11-18 17:23:17 -0600

    cleaned up nav. Need to create links for Order button and fix landing page.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -26,75 +26,91 @@ const Nav = (props) => {
   return (
     <div className="nav">
 
-  <AppBar position="static">
-    <Toolbar color="inherit">
-      <Grid 
-      justify="space-between"
-      container 
-      spacing ={24}>
-        <Grid item>
-            <IconButton>
-              <MenuIcon />
-            </IconButton>
-            <Link to="/home">
-              <h2 className="nav-title">Smokin' Ninjas</h2>
-            </Link>
+      <AppBar position="static">
+        <Toolbar color="inherit">
+          <Grid
+            justify="space-between"
+            container
+            spacing={24}>
+            <Grid item>
+              <IconButton>
+                <MenuIcon />
+              </IconButton>
             </Grid>
-      </Grid>
-      <Grid 
+            <Grid item>
+              <Typography>
+                <Link to="/home">
+                  <h2 className="nav-title">Smokin' Ninjas</h2>
+                </Link>
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button color="inherit">
+                Order Online
+          </Button>
+            </Grid>
+          </Grid>
+          {/* <Grid 
       item 
       color="inherit" >
             <div className="nav-right">
-              <Link className="nav-link" to={loginLinkData.path}>
-                {/* Show this link if they are logged in or not,
+              <Link className="nav-link" to={loginLinkData.path}> */}
+          {/* Show this link if they are logged in or not,
           but call this link 'Home' if they are logged in,
           and call this link 'Login / Register' if they are not */}
-                {loginLinkData.text}
-              </Link>
-              {/* Show the link to the info page and the logout button if the user is logged in */}
-              {props.store.user.id && (
+          {/* {loginLinkData.text}
+              </Link> */}
+          {/* Show the link to the info page and the logout button if the user is logged in */}
+          {/* {props.store.user.id && (
                 <>
                   <Link className="nav-link" to="/info">
                     Info Page
             </Link>
                   <LogOutButton className="nav-link" />
                 </>
-              )}
-              {/* Always show this link since the about page is not protected */}
-              {/* <Link className="nav-link" to="/about">
+              )} */}
+          {/* Always show this link since the about page is not protected */}
+          {/* <Link className="nav-link" to="/about">
           About
         </Link> */}
-            </div>
-      </Grid>
-    </Toolbar>
-  </AppBar>
+          {/* </div>
+      </Grid> */}
+          {/* <Grid>
+        <Typography>
+          <Button color="inherit">
+            Order Online
+          </Button>
+        </Typography>
+      </Grid> */}
+        </Toolbar>
+      </AppBar>
 
-    {/* <div className="nav">
+      {/* <div className="nav">
       <Link to="/home">
         <h2 className="nav-title">Smokin' Ninjas</h2>
       </Link>
       <div className="nav-right">
         <Link className="nav-link" to={loginLinkData.path}> */}
-          {/* Show this link if they are logged in or not,
+      {/* Show this link if they are logged in or not,
           but call this link 'Home' if they are logged in,
         //   and call this link 'Login / Register' if they are not */}
-        {/* //   {loginLinkData.text}
+      {/* //   {loginLinkData.text}
         // </Link> */}
-        {/* Show the link to the info page and the logout button if the user is logged in */}
-        {/* // {props.store.user.id && ( */}
-        {/* //   <>
+      {/* Show the link to the info page and the logout button if the user is logged in */}
+      {/* // {props.store.user.id && ( */}
+      {/* //   <>
         //     <Link className="nav-link" to="/info">
         //       Info Page
         //     </Link>
         //     <LogOutButton className="nav-link" />
         //   </>
         // )} */}
-        {/* Always show this link since the about page is not protected */}
-    {/* //     <Link className="nav-link" to="/about">
+      {/* Always show this link since the about page is not protected */}
+      {/* //     <Link className="nav-link" to="/about">
     //       About
     //     </Link>
     //   </div> */}
-    {/* // </div> */}
+      {/* // </div> */}
     </div>
   );
 };
