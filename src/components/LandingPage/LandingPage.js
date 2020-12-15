9db8201137c6db866cf9f015b4235bb49647d9fd
Tou Xiong <touyeexiong@gmail.com>
2020-12-14 23:33:15 -0600

    added links to social media buttons. Need to style slider, may end up using old slider CarouselProvider.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -9,19 +9,12 @@ import Grid from "@material-ui/core/Grid";
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import { Twitter, Instagram } from '@material-ui/icons';
+import { Twitter, Instagram, Facebook } from '@material-ui/icons';
 
 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
-import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: theme.spacing(2),
-  }
-}))
 
 class LandingPage extends Component {
   state = {
@@ -32,7 +25,11 @@ class LandingPage extends Component {
     this.props.history.push('/login');
   };
 
-  
+  handleSocial = (url) => {
+    let newWindow = window.open(url, '_blank', 'noopener, noreferrer')
+    if (newWindow) newWindow.opener = null;
+  }
+
 
   render() {
     const settings = {
@@ -44,33 +41,81 @@ class LandingPage extends Component {
     };
 
     const Button = styled.button`
-    color: palevioletred;
+    color: primary;
     `
-
-    const classes = useStyles();
 
     return (
       <>
-      <div>
-        <Grid>
-          <h2> Single Item</h2>
-          <Slider {...settings}>
-            <div>
-              <img src="http://placekitten.com/g/400/200" />
-            </div>
-            <div>
-              <img src="http://placekitten.com/g/400/200" />
-            </div>
-            <div>
-              <img src="http://placekitten.com/g/400/200" />
-            </div>
-            <div>
-              <img src="http://placekitten.com/g/400/200" />
-            </div>
-          </Slider>
+        <div>
+          <Grid className="grid-col">
+            <h2> Single Item</h2>
+            <Slider {...settings}>
+              <div>
+                <img src="http://placekitten.com/g/400/200" />
+              </div>
+              <div>
+                <img src="http://placekitten.com/g/400/200" />
+              </div>
+              <div>
+                <img src="http://placekitten.com/g/400/200" />
+              </div>
+              <div>
+                <img src="http://placekitten.com/g/400/200" />
+              </div>
+            </Slider>
           </Grid>
         </div>
-      <div className="container">
+        <div className="">
+          <div className="grid">
+            <div className="grid-col grid-col_8">
+              <p>
+                Testing testing 123
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra
+                lacus ut ex molestie blandit. Etiam et turpis sit amet risus
+                mollis interdum. Suspendisse et justo vitae metus bibendum
+                fringilla sed sed justo. Aliquam sollicitudin dapibus lectus,
+                vitae consequat odio elementum eget. Praesent efficitur eros vitae
+                nunc interdum, eu interdum justo facilisis. Sed pulvinar nulla ac
+                dignissim efficitur. Quisque eget eros metus. Vestibulum bibendum
+                fringilla nibh a luctus. Duis a sapien metus.
+            </p>
+            </div>
+          </div>
+          <div>
+            <Link
+              to="/menu"
+            >
+              <Button
+                primary
+                variant="contained"
+              >
+                View Our Menu
+        </Button>
+            </Link>
+          </div>
+
+
+        </div>
+
+        <AppBar
+          position="static"
+        >
+          <Grid
+            justify="space-evenly"
+            container
+            direction="row"
+            alignItems="center"
+          >
+            <Instagram style={{ fontSize: 40 }} onClick={() => this.handleSocial('https://google.com')}></Instagram>
+            <Facebook style={{ fontSize: 40 }} onClick={() => this.handleSocial('https://facebook.com')}></Facebook>
+          </Grid>
+        </AppBar>
+        <div className="center"
+        >
+          <h1>Contact & Opening Times</h1>
+
+        </div>
         <div className="grid">
           <div className="grid-col grid-col_8">
             <p>
@@ -87,31 +132,6 @@ class LandingPage extends Component {
             </p>
           </div>
         </div>
-        <Link 
-        to="/menu"
-        >
-            <Button
-              primary
-              variant="contained"
-            >
-              View Our Menu
-        </Button>
-        </Link>
-
-      </div>
-      <Grid
-      justify="space-between"
-      container
-      spacing={24}
-      >
-          <AppBar 
-          position="static"
-          style={classes.root}
-          >
-            <Instagram fontSize="large"></Instagram>
-            <Twitter fontSize="large"></Twitter>
-          </AppBar>
-      </Grid>
       </>
     );
   }
