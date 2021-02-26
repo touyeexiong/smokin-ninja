030a3506782e64e430f63472eee448c27a7b468e
Tou Xiong <touyeexiong@gmail.com>
2021-02-25 21:28:05 -0600

    placing menu data into grids. Converting functional components over to class, running into issues with Material-Ui.

--- src/components/OrderOnline/OrderOnline.jsx
+++ src/components/OrderOnline/OrderOnline.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import mapStoreToProps from "../../redux/mapStoreToProps"
 import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 
 import { connect } from 'react-redux';
 
