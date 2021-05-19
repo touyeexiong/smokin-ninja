0a16bed0aa2b8314df673ebd2af3e1d6cd1badca
Tou Xiong <touyeexiong@gmail.com>
2021-05-18 22:50:29 -0500

    removed container spacing for menuitems

--- src/components/OrderOnline/OrderOnline.jsx
+++ src/components/OrderOnline/OrderOnline.jsx
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import OrderOnlineMenu from '../OrderOnlineMenu/OrderOnlineMenu'
-// import {
-//     HashRouter as Router,
-//     Route,
-//     Redirect,
-//     Switch,
-// } from 'react-router-dom';
 import mapStoreToProps from "../../redux/mapStoreToProps"
 import Grid from '@material-ui/core/Grid'
-// import Paper from '@material-ui/core/Paper'
 import './OrderOnline.css';
 
 import { connect } from 'react-redux';
@@ -23,11 +16,11 @@ class OrderOnline extends Component {
         return (
             <>
             <h1>
-                <Grid>
+                <Grid classname="grid">
                         {this.props.store.getMenu.map((items) => {
                         return (
                             <>
-                                <Grid container spacing={3} className="grid-col">
+                                <Grid  className="grid-col">
                             <OrderOnlineMenu id={items.id} name={items.name} price={items.price}/>
                                 </Grid>
                             </>
--- src/components/OrderOnlineMenu/OrderOnlineMenu.jsx
+++ src/components/OrderOnlineMenu/OrderOnlineMenu.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-// import Typography from '@material-ui/core/Typography';
-// import ButtonBase from '@material-ui/core/ButtonBase';
 import './OrderOnlineMenu.css';
 
 class OrderOnlineMenu extends Component {
