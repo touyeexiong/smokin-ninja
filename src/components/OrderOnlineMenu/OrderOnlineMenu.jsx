94c35d7aee289fc83955f0b6d1eaf92ea4cd8b7f
Tou Xiong <touyeexiong@gmail.com>
2021-05-24 23:49:33 -0500

    need to use value for ToggleButton not selected. Need to find way to toggle multiple menuitems on and off.

--- src/components/OrderOnlineMenu/OrderOnlineMenu.jsx
+++ src/components/OrderOnlineMenu/OrderOnlineMenu.jsx
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './OrderOnlineMenu.css';
-import Paper from '@material-ui/core/Paper'
+import Paper from '@material-ui/core/Paper';
 import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 class OrderOnlineMenu extends Component {
     state = {
-        selected: '',
+        selected: "",
     };
 
     handleSelection = () => {
         console.log(`you clicked`, this.props.id);
+        // if (this.state.selected = '') {
+        //     this.setState({
+        //         selected: 'selected'
+        //     })
+        // } else {
+        //     this.setState({
+        //         selected: ''
+        //     })
+        // }
         this.setState({
-            selected: 'selected'
+            selected: "apples"
         })
+        console.log(this.state.selected);
+        
     }
 
     render () {
         return (
             <>
-                <ToggleButton selected={this.state.selected} onClick={this.handleSelection}>
+            <ToggleButtonGroup  onClick={this.handleSelection} value={this.state.selected}>
+                    <ToggleButton value="apples">
+                    Testing
+                </ToggleButton>
+                
+            </ToggleButtonGroup>
+                <ToggleButton onClick={this.handleSelection}>
                     <Paper elevation={5} onClick={this.handleSelection} className="paper">
                         <div className="grid" flexGrow={1} id={this.props.id}>{this.props.name} {this.props.price}</div>
                     </Paper>
