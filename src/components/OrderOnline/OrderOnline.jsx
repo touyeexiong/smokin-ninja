688422d2a5b64001892cc1c288c4db18e40e8718
Tou Xiong <touyeexiong@gmail.com>
2021-08-24 14:43:35 -0500

    fixed grid styling. Had too many Grid Components. Confusing nesting Grids.

--- src/components/OrderOnline/OrderOnline.jsx
+++ src/components/OrderOnline/OrderOnline.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import OrderOnlineMenu from '../OrderOnlineMenu/OrderOnlineMenu'
-import mapStoreToProps from "../../redux/mapStoreToProps"
-import Grid from '@material-ui/core/Grid'
-import './OrderOnline.css'
+import OrderOnlineMenu from '../OrderOnlineMenu/OrderOnlineMenu';
+import mapStoreToProps from "../../redux/mapStoreToProps";
+import Grid from '@material-ui/core/Grid';
+import '../LandingPage/LandingPage.css';
 import { connect } from 'react-redux';
 class OrderOnline extends Component {
 
@@ -15,16 +15,13 @@ class OrderOnline extends Component {
             <>
                 <div className="grid">
                     <div className="grid-col grid-col_8">
-                    <Grid container spacing={1}>
+                        <Grid justify="space-evenly" container item spacing={3}>
                             {this.props.store.getMenu.map((items) => {
                                 return (
-                                    <Grid container item xs={3} spacing={3}>
                                         <OrderOnlineMenu key={items.id} id={items.id} name={items.name} price={items.price} />
-                                    </Grid>
                                 )
                             })}
                     </Grid>
-                    <h1 className="center">Hello</h1>
                 </div>
             </div>
             </>
--- src/components/OrderOnlineMenu/OrderOnlineMenu.jsx
+++ src/components/OrderOnlineMenu/OrderOnlineMenu.jsx
@@ -11,7 +11,7 @@ class OrderOnlineMenu extends Component {
     render () {
         return (
             <>
-                    <Grid className="grid-col grid-col_8" display="flex" flexWrap="wrap" item xs={4}>
+                    <Grid className="grid-col grid-col_8" display="flex" flexWrap="wrap" item xs={3}>
                         <Paper className="paper" flex={1} elevation={5}>
                             {this.props.name} {this.props.price}
                         </Paper>                    
